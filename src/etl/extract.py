f60446a147aacc8ed9b9a435f2e214318793813f
Oleksii Yefremov <alexey.efremov333@gmail.com>
2024-12-21 21:59:40 +0200

    update: add random email generation to etl.generator, update logger

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -3,6 +3,8 @@ import pandas as pd
 from utils.logging_config import logging
 from utils.data_config import data_path, csv_path
 from pathlib import Path
+import random
+
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +22,7 @@ def generator():
         for i in range(1, 1002):
             data.loc[i, 'user_id'] = i
             data.loc[i, 'name'] = fake.name()
-            data.loc[i, 'email'] = fake.free_email()
+            data.loc[i, 'email'] = random.choice((fake.free_email(), fake.email(), fake.domain_name()))
             data.loc[i, 'signup_date'] = fake.date_time_this_year()
 
         data['user_id'] = data['user_id'].astype(int)
@@ -32,18 +34,16 @@ def generator():
 def extractor():
     """
     Extracts data from .csv file
-    :return:
+    :return: data — pandas.DataFrame object from data.csv
     """
+    logger.info('Starting to extract data...')
+
     if not Path(csv_path).exists():
         generator()
 
-    logger.info('Starting to extract data...')
-
     try:
         data = pd.read_csv(csv_path)
+        logger.info('Extracting completed.')
         return data
     except Exception as e:
         logger.error(f'Got an error: {e}\n')
-
-if __name__ == "__main__":
-    extractor()
