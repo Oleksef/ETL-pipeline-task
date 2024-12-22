86c8858213f8279cfbbe25a12d91a9bd3f40b9e0
Oleksii Yefremov <alexey.efremov333@gmail.com>
2024-12-22 03:35:31 +0200

    feat: add the path for transformed data

--- src/utils/data_config.py
+++ src/utils/data_config.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 data_path = os.getenv('CSV_PATH', Path(__file__).parents[1] / 'data')
 csv_path = str(data_path) + '\\data.csv'
+transformed_csv_path = str(data_path) + '\\transformed_data.csv'
